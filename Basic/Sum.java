14c95e04d9a1ad6d61c2dfeb08fe29c201ed5813
Vishal <vishal012mane@gmail.com>
2025-07-23 18:45:45 +0530

    Add functionality to calculate and display the sum of two numbers

--- Basic/Sum.java
+++ Basic/Sum.java
@@ -1,5 +1,15 @@
+package Basic;
+import java.util.Scanner;
+
 public class Sum {
     public static void main(String[] args){
-        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter first number:");
+        int num1 = input.nextInt();
+        System.out.print("Enter second number:");
+        int num2 = input.nextInt();
+
+        int sum = num1 + num2;
+        System.out.println("Sum of "+ num1 +" and " +num2 +" is "+sum);
     }
 }
