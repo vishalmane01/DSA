c5f82f85793a5900e8eb0101173ed960e4c40955
Vishal <vishal012mane@gmail.com>
2025-07-25 22:10:10 +0530

    fibonacci

---
+++ Basic/Fibonacci.java
@@ -0,0 +1,20 @@
+package Basic;
+import java.util.Scanner;
+public class Fibonacci {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter number of terms: ");
+        int terms = input.nextInt();
+
+        int first = 0 , second = 1, next;
+
+        for (int i = 1; i<=terms; i++){
+            System.out.print(first+" ");
+            next = first + second;
+            first = second;
+            second = next;
+            
+        }
+    }
+}
