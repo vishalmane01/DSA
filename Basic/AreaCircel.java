fe8a6402f0640f5022e2f7fe0c98d1e29db3c1ce
Vishal <vishal012mane@gmail.com>
2025-07-27 10:40:39 +0530

    input

--- Basic/AreaCircel.java
+++ Basic/AreaCircel.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class AreaCircel {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         
         System.out.print("Enter the radius of the circle: ");
-        double radius = sc.nextDouble();
+        double radius = input.nextDouble();
         double area = Math.PI * radius * radius;
         
         System.out.println("Area of the circle is: " + area);
