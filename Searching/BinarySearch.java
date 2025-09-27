a31884dcc1abba4d4dabcc2065be0ccf4097517d
Vishal <vishal012mane@gmail.com>
2025-09-27 15:47:41 +0530

    Binary Search

---
+++ Searching/BinarySearch.java
@@ -0,0 +1,31 @@
+package Searching;
+
+public class BinarySearch {
+    public static void main(String[] args){
+
+        int[] nums = {1,2,3,4,5,6,7,8,9,10};
+        int target = 9;
+
+        int ans = binarySearch(nums, target);
+        System.out.println(ans);
+    }
+
+    static int binarySearch(int[] arr, int target){
+        int start = 0;
+        int end = arr.length -1;
+
+        while(start <= end){
+            //find middle element
+            int mid = start + (end - start) / 2;
+            if(target < arr[mid]){
+                end = mid -1;
+            }else if(target > arr[mid]){
+                start = mid + 1;
+            }else{
+                return mid;
+            }
+        }
+
+        return -1;
+    }
+}
---
+++ Searching/LinearSearch.java
@@ -0,0 +1,25 @@
+package Searching;
+
+public class LinearSearch {
+    public static void main(String[] args){
+        int[] nums = {2,4,0,1,9};
+        int target = 9;
+
+        int ans = linearSearch(nums, target); 
+        System.out.println(ans);
+    }
+
+    static int linearSearch(int[] arr, int target){
+        if(arr.length == 0){
+            return -1;
+        }
+
+        for(int element : arr){
+            if(element == target){
+                return element;
+            }
+        }
+
+        return -1; 
+    }
+}
