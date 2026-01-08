6f5dcabd0c6d518cc2b22d31065e923e624b919f
Vishal <vishal012mane@gmail.com>
2026-01-08 16:04:04 +0530

    LC examples of cyclic sort

---
+++ Sorting/LC448.java
@@ -0,0 +1,48 @@
+
+// 448. Find All Numbers Disappeared in an Array
+// Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
+
+// Example 1:
+// Input: nums = [4,3,2,7,8,2,3,1]
+// Output: [5,6]
+
+// Example 2:
+// Input: nums = [1,1]
+// Output: [2]
+
+package Sorting;
+import java.util.List;
+
+public class LC448 {
+ public static void main(String[] args){
+     int[] nums = {4,3,2,7,8,2,3,1};
+     List<Integer> ans = findDisappearedNumbers(nums);
+     System.out.println(ans);
+ }  
+ 
+public static List<Integer> findDisappearedNumbers(int[] nums){
+    int i=0;
+    while(i < nums.length){
+        int correct = nums[i] - 1;
+        if(nums[i] != nums[correct]){
+            swap(nums, i , correct);
+        }else{
+            i++;
+        }
+    }
+List<Integer> ans = new java.util.ArrayList<>();
+for (int index = 0; index < nums.length; index++){
+    if(nums[index] != index+1){
+        ans.add(index+1);
+    }
+}
+return ans;
+
+} 
+
+static void swap(int[] nums, int first, int second){
+    int temp = nums[first];
+    nums[first] = nums[second];
+    nums[second] = temp;
+}
+}
