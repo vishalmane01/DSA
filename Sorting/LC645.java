6b6484a71315bb3e69ae4dac09b9c5c41306692a
Vishal <vishal012mane@gmail.com>
2026-01-09 15:39:16 +0530

    LC645

---
+++ Sorting/LC645.java
@@ -0,0 +1,26 @@
+package Sorting;
+
+public class LC645 {
+    public int[] findErrorNums(int[] nums) {
+            int i = 0;
+            while(i < nums.length){
+                int correct = nums[i] - 1;
+                if(nums[i] != nums[correct]){
+                    int temp = nums[i];
+                    nums[i] = nums[correct];
+                    nums[correct] = temp; 
+                }else{
+                    i++;
+                }
+            }
+
+            for(int index = 0; index < nums.length ; index++){
+                if(nums[index] != index + 1){
+                    return new int[] {nums[index] , index +1 }; 
+                }
+            }
+
+        return new int[] {-1 , -1};
+    }
+
+}
